35619abed4224aad9157481cb4ea5cc4e39964cc
Sergey Bovyrin <sbovyrin@gmail.com>
2021-06-10 04:31:03 +0300

    Update: fix bugs and improvements
    - Lead func's args to stable state
    - Add funcs - `apply`/`applyTo`/`spread`/`forEvery`/`either`
    - Fix `map` to support multiple args start from second place

--- src/std.php
+++ src/std.php
@@ -22,10 +22,24 @@ if (!function_exists('flip')) {
     }
 }
 
+if (!function_exists('apply')) {
+    function apply(callable $f): callable
+    {
+        return static fn ($x = null) => $f($x);
+    }
+}
+
+if (!function_exists('applyTo')) {
+    function applyTo($x = null): callable
+    {
+        return static fn (callable $f) => $f($x);
+    }
+}
+
 if (!function_exists('compose')) {
     function compose($f, $g): callable
     {
-        return static fn ($x) => $f($g($x));
+        return static fn ($x = null) => $f($g($x));
     }
 }
 
@@ -122,14 +136,21 @@ if (!function_exists('isObject')) {
 }
 
 if (!function_exists('getOr')) {
-    function getOr($x, $z) {
-        return isNone($x) ? $z : $x;
+    function getOr($x): callable {
+        return static fn ($z) => isNone($x) ? $x : $z;
     }
 }
 
 //
 // Logic
 //
+if (!function_exists('either')) {
+    function either(callable $p, callable $f, callable $g): callable
+    {
+        return static fn($x) => $p($x) ? $f($x) : $g($x);
+    }
+}
+
 if (!function_exists('eq')) {
     function eq($x, $z): bool {
         return $x === $z;
@@ -254,57 +275,61 @@ if (!function_exists('reverse')) {
 }
 
 if (!function_exists('path')) {
-    function path($path, $xs)
+    function path($path): callable
     {
-        if (!(isList($xs) || isDict($xs) || isString($xs))) {
-            raiseTypeErr('expected list/dict or string', 2);
-        }
+        return static function ($xs) use ($path) {
+            if (!(isList($xs) || isDict($xs) || isString($xs))) {
+                raiseTypeErr('expected list/dict or string', 2);
+            }
 
-        if (isString($xs) && !isInt($path)) {
-            raiseTypeErr('expected int when arg2 is string', 1);
-        }
+            if (isString($xs) && !isInt($path)) {
+                raiseTypeErr('expected int when arg2 is string', 1);
+            }
 
-        switch (true) {
-            case isList($path):
-                $_xs = &$xs;
-                foreach ($path as $k) {
-                    if (isset($_xs[$k])) $_xs = &$_xs[$k];
-                    else return null;
-                }
-                return $_xs;
-            case isString($path) || isInt($path): return $xs[$path] ?? null;
-            default: raiseTypeErr('expected string/integer or list', 1);
-        }
+            switch (true) {
+                case isList($path):
+                    $_xs = &$xs;
+                    foreach ($path as $k) {
+                        if (isset($_xs[$k])) $_xs = &$_xs[$k];
+                        else return null;
+                    }
+                    return $_xs;
+                case isString($path) || isInt($path): return $xs[$path] ?? null;
+                default: raiseTypeErr('expected string/integer or list', 1);
+            }
+        };
     }
 }
 
 if (!function_exists('assoc')) {
-    function assoc($path, callable $f, $xs)
+    function assoc($path, callable $f): callable
     {
-        if (!(isInt($path) || isString($path) || isList($path)))
-            raiseTypeErr('expected string/int or list', 1);
+        return static function ($xs) use ($path, $f): array {
+            if (!(isInt($path) || isString($path) || isList($path)))
+                raiseTypeErr('expected string/int or list', 1);
 
-        if (!isList($path)) $path = [$path];
+            if (!isList($path)) $path = [$path];
 
-        $_assoc = static function ($v) use ($path, $f) {
-            $_xs = &$v;
-            foreach ($path as $k) {
-                if (!isset($_xs[$k])) $_xs[$k] = [];
-                $_xs = &$_xs[$k];
+            $_assoc = static function ($v) use ($path, $f) {
+                $_xs = &$v;
+                foreach ($path as $k) {
+                    if (!isset($_xs[$k])) $_xs[$k] = [];
+                    $_xs = &$_xs[$k];
+                }
+                $_xs = $f($_xs);
+
+                return $v;
+            };
+
+            switch (true) {
+                case isList($xs) || isDict($xs): return $_assoc($xs);
+                case isString($xs):
+                    if (!isInt($path))
+                        raiseTypeErr('expected int when arg3 is string', 1);
+                    return join("", $_assoc(str_split($xs)));
+                default: raiseTypeErr('expected list/dict or string', 3);
             }
-            $_xs = $f($_xs);
-
-            return $v;
         };
-
-        switch (true) {
-            case isList($xs) || isDict($xs): return $_assoc($xs);
-            case isString($xs):
-                if (!isInt($path))
-                    raiseTypeErr('expected int when arg3 is string', 1);
-                return join("", $_assoc(str_split($xs)));
-            default: raiseTypeErr('expected list/dict or string', 3);
-        }
     }
 }
 
@@ -371,57 +396,69 @@ if (!function_exists('keys')) {
     }
 }
 
-if (!function_exists('fold')) {
-    function fold(callable $f, $v, array $xs)
+if (!function_exists('forEvery')) {
+    function forEvery(callable $f): callable
     {
-        $x = $v;
-        foreach ($xs as $key => $val) {
-            $x = $f($x, $val, $key);
-        }
-        return $x;
+        return static function (array $xs) use ($f) {
+            foreach ($xs as $key => $val) {
+                $f($val, $key);
+            }
+        };
+    }
+}
+
+if (!function_exists('fold')) {
+    function fold(callable $f, $v): callable
+    {
+        return static function (array $xs) use ($f, $v) {
+            $x = $v;
+            foreach ($xs as $key => $val) {
+                $x = $f($x, $val, $key);
+            }
+            return $x;
+        };
     }
 }
 
 if (!function_exists('reduce')) {
-    function reduce(callable $f, array $xs)
+    function reduce(callable $f): callable
     {
-        if (eq(len($xs), 0)) raiseTypeErr('expected not empty list/dict', 2);
-        return fold($f, head($xs), tail($xs));
+        return static function (array $xs) use ($f) {
+            if (eq(len($xs), 0)) raiseTypeErr('expected not empty list/dict', 2);
+            return fold($f, head($xs))(tail($xs));
+        };
     }
 }
 
 if (!function_exists('filter')) {
-    function filter(callable $p, array $xs): array
+    function filter(callable $p): callable
     {
         return fold(
             static function ($x, $v, $k) use ($p) {
                 if ($p($v, $k)) $x[$k] = $v;
                 return $x;
             },
-            [],
-            $xs
+            []
         );
     }
 }
 
 if (!function_exists('any')) {
-    function any(callable $p, array $xs): bool
+    function any(callable $p): callable
     {
         return fold(
             static fn ($x, $v, $k) => $x || $p($v, $k),
-            false,
-            $xs
+            false
         );
     }
 }
 
 if (!function_exists('all')) {
-    function all(callable $p, array $xs): bool
+    function all(callable $p)
     {
         return fold(
             static fn ($x, $v, $k) => $x && $p($v, $k),
-            true,
-            $xs
+            true
         );
     }
 }
@@ -429,7 +466,7 @@ if (!function_exists('all')) {
 if (!function_exists('comp')) {
     function comp(...$fs): callable
     {
-        return fold('compose', 'id', $fs);
+        return fold('compose', 'id')($fs);
     }
 }
 
@@ -437,9 +474,9 @@ if (!function_exists('concat')) {
     function concat(...$xs)
     {
         switch (true) {
-            case all('isString', $xs): return join("", $xs);
-            case all(static fn ($v) => isList($v) || isDict($v), $xs):
-                return reduce('array_merge', $xs);
+            case all('isString')($xs): return join("", $xs);
+            case all(static fn ($v) => isList($v) || isDict($v))($xs):
+                return reduce(static fn ($a, $b) => array_merge($a, $b))($xs);
             default:
                 raiseTypeErr('expected all args either list/dict or string', 1);
         }
@@ -447,16 +484,16 @@ if (!function_exists('concat')) {
 }
 
 if (!function_exists('map')) {
-    function map(callable $f, array $xs): array
+    function map(callable $f): callable
     {
-        return fold(
-            static function ($x, $y) use ($f) {
-                $x[] = $f($y);
-                return $x;
-            },
-            [],
-            $xs
-        );
+        return static fn (...$xs): array =>
+            fold(
+                static function ($x, $n) use ($f, $xs) {
+                    $x[] = $f(...array_column($xs, $n));
+                    return $x;
+                },
+                []
+            )(range(0, min(array_map('count', $xs)) - 1));
     }
 }
 
@@ -483,8 +520,8 @@ if (!function_exists('uniq')) {
     {
         switch (true) {
             case isString($xs):
-                return join('', array_values(array_unique(str_split($xs))));
-            case isList($xs): return array_values(array_unique($xs));
+                return join('', vals(array_unique(str_split($xs))));
+            case isList($xs): return vals(array_unique($xs));
             case isDict($xs): return array_unique($xs);
             default: raiseTypeErr('expected list/dict or string', 1);
         }
@@ -502,9 +539,9 @@ if (!function_exists('has')) {
         $_listCase = static fn ($_x) => in_array($_x, $xs, true);
 
         switch (true) {
-            case isString($xs) && isList($x): return all($_strCase, uniq($x));
+            case isString($xs) && isList($x): return all($_strCase)(uniq($x));
             case isString($xs): return $_strCase($x);
-            case isList($xs) && isList($x): return all($_listCase, uniq($x));
+            case isList($xs) && isList($x): return all($_listCase)(uniq($x));
             case isList($xs): return $_listCase($x);
             default: raiseTypeErr('expected list/dict or string', 2);
         }
@@ -523,8 +560,7 @@ if (!function_exists('chunk')) {
                             ? [...$a, [$b]]
                             : [...init($a), [...last($a), $b]],
                     [],
-                    $xs
-                );
+                )($xs);
             default: raiseTypeErr('expected list or string', 2);
         }
     }
@@ -542,11 +578,10 @@ if (!function_exists('pluck')) {
 if (!function_exists('pick')) {
     function pick(array $keys, array $xs): array
     {
-        if (isList($xs) && (all('isDict', $xs) || all('isList', $xs))) {
+        if (isList($xs) && (all('isDict')($xs) || all('isList')($xs))) {
             return map(
                 partial('filter', static fn ($v, $k) => has($k, $keys)),
-                $xs
-            );
+            )($xs);
         }
 
         raiseTypeErr('expected nested list or dict', 2);
@@ -578,6 +613,13 @@ if (!function_exists('intersect')) {
             default:
                 raiseTypeErr('expected list/dict and the same type as arg 1', 2);
         }
+    }
+}
+
+if (!function_exists('spread')) {
+    function spread(callable $f): callable
+    {
+        return static fn (array $xs) => $f(...$xs);
     }
 }
 
